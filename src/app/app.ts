90e7ed86d400c28dca6a469df4748f1bd8d9de66
tonkla785 <tonkla785@gmail.com>
2025-08-18 04:10:59 +0700

    Angular_Learning

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,71 @@
+import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('Learn-angular');
+  name = "Kla Phichitpon"
+  age = 24
+  address = "chantuburi"
+
+  imageurl = "https://picsum.photos/id/27/367/267"
+  size = 200
+
+  username = "admin"
+  userPermission = "admin"
+
+  isLogin = true
+
+  data =[
+    { id: 1, name: "Kla",  salary: 25000 },
+    { id: 2, name: "Phichitpon", salary: 22000 },
+    { id: 3, name: "Kla Phichitpon", salary: 27000 },
+    { id: 4, name: "AKA", salary: 30000 }
+  ]
+
+  isActive = false
+
+  clearData() {
+    this.data = []
+  }
+
+  showAddress() {
+    return "current address " + this.address
+  }
+
+  doubleSize() {
+    return this.size * 2
+  }
+
+  showMessage() {
+    alert("Hello World " + this.name)
+    console.log("Message")
+  }
+
+  toggle() {
+  this.isLogin = !this.isLogin;
+  }
+
+  changePermission() {
+    if (this.userPermission === "admin") {
+      this.userPermission = "manager";
+    } else if(this.userPermission === "manager") {
+      this.userPermission = "customer";
+    }else if(this.userPermission === "customer") {
+      this.userPermission = "kla";
+    }else {
+      this.userPermission = "admin";
+    }
+  }
+
+  toggleMode() {
+    this.isActive = !this.isActive;
+  }
 }
